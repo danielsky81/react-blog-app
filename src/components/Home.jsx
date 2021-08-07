643c0c91e0ca2bffce0d3d34bc3daf3cf0c9de5e
Daniel Orchel <karma0rg4u@gmail.com>
2021-08-07 22:07:15 +0100

    Add fetch with catch errors and state changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,36 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
-
   useEffect(() => {
-
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if (!res.ok) {
+          throw Error('Could not fetched the data. Sorry!')
+        }
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+        setIsPending(false);
+      })
+    }, 1000)
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title='Marios Blogs' handleDelete={handleDelete}/>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title='All Blogs'/>}
     </div>
   )
 }
